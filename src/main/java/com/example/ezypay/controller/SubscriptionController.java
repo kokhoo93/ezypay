ce29874acf6dcfa6ee47bd594e580fd9e5819fc1
Kok Hoo <kokhoo.octafa@gmail.com>
2020-03-16 14:54:46 +0800

    update

--- src/main/java/com/example/ezypay/controller/SubscriptionController.java
+++ src/main/java/com/example/ezypay/controller/SubscriptionController.java
@@ -3,6 +3,7 @@ package com.example.ezypay.controller;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,7 @@ public class SubscriptionController {
 	@Autowired
 	private SubscriptionService subscriptionService;
 	
+	@CrossOrigin(origins = "https://ezypay-vue.herokuapp.com/")
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
     @ResponseBody
     public Subscription createSubscription(@RequestBody Subscription subs) throws ParseException {
